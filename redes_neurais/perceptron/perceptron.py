e99d7b1d7d67b1c48272bbf676935256e680beef
Senechal <pumba.usp@gmail.com>
2016-04-14 12:16:17 -0300

    Add bias to perceptron.

--- redes_neurais/perceptron/perceptron.py
+++ redes_neurais/perceptron/perceptron.py
@@ -7,9 +7,10 @@ class Perceptron(object):
     _input = numpy.matrix([])
     _trained_w = numpy.array([])
 
-    def __init__(self, oracle, label = ""):
+    def __init__(self, oracle, label = "", bias = -1):
         self._oracle = numpy.array(oracle)
         self._label = label
+        self.bias = bias
 
     @property
     def x(self):
@@ -25,12 +26,20 @@ class Perceptron(object):
     def train_neuron(self, n, ni=0.1):
         w = n.w
         for index, row in enumerate(self._input.A):
-            v = n.run(row.tolist())
+            bias_row = numpy.append([self.bias], row)
+            v = n.run(bias_row.tolist())
             y = Neuron.step(v)
-            n.w = self._recalc_w(n.w, ni, self._oracle[index], y, row)
+            n.w = self._recalc_w(n.w, ni, self._oracle[index], y, bias_row)
         if numpy.array_equal(n.w, w):
             return n
         else:
             return self.train_neuron(n, ni)
 
+    def test(self, n, tests):
+        data = numpy.matrix(tests)
+        for row in data.A:
+            bias_row = numpy.append([self.bias], row)
+            v = n.run(bias_row.tolist())
+            y = Neuron.step(v)
+            print "Testing {0}: output:{1}".format(row,y)
 
